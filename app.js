7413e124c16919ca4308a609e5ad65234a40d83a
TitoBerru <c_myco@hotmail.com>
2021-06-14 16:16:26 -0300

    requeri el ejs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require ('path');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 const publicPath = path.resolve(__dirname,'./public');
 app.use( express.static(publicPath));

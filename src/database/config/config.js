91302937db4c19b60f32f3381a76c10325b900b4
TitoBerru <c_myco@hotmail.com>
2021-08-16 17:53:02 -0300

    link heroku - alwaysdata

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,18 @@
 
 module.exports = {
+  // "development": {
+  //   "username": "root",
+  //   "password": null,
+  //   "database": "yotemudo",
+  //   "host": "127.0.0.1",
+  //   "dialect": "mysql",
+  //   "port": 3306
+  // },
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "yotemudo",
-    "host": "127.0.0.1",
+    "username": "241156_user",
+    "password": "User*2021",
+    "database": "proyectointegrador_8",
+    "host": "mysql-proyectointegrador.alwaysdata.net",
     "dialect": "mysql",
     "port": 3306
   },

334d76351b283a070e5b3cb465918f211d756b0f
TitoBerru <c_myco@hotmail.com>
2021-08-16 18:12:08 -0300

    corrijo error para heroku

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -24,10 +24,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": "241156_user",
+    "password": "User*2021",
+    "database": "proyectointegrador_8",
+    "host": "mysql-proyectointegrador.alwaysdata.net",
+    "dialect": "mysql",
+    "port": 3306
   }
 }

3f2040feefde1e0cd13fb5ca339a88c11f957e95
TitoBerru <c_myco@hotmail.com>
2021-08-18 00:15:48 -0300

    configuracion de config para database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,18 +1,12 @@
 
 module.exports = {
-  // "development": {
-  //   "username": "root",
-  //   "password": null,
-  //   "database": "yotemudo",
-  //   "host": "127.0.0.1",
-  //   "dialect": "mysql",
-  //   "port": 3306
-  // },
+
+  // Esta configuracion apunta al local
   "development": {
-    "username": "241156_user",
-    "password": "User*2021",
-    "database": "proyectointegrador_8",
-    "host": "mysql-proyectointegrador.alwaysdata.net",
+    "username": "root",
+    "password": null,
+    "database": "yotemudo",
+    "host": "127.0.0.1",
     "dialect": "mysql",
     "port": 3306
   },
@@ -23,6 +17,7 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
+  // Esta configuracion apunta a AlwaysData....********¡¡¡¡ Comentarla Por favor!!!! ********
   "production": {
     "username": "241156_user",
     "password": "User*2021",
